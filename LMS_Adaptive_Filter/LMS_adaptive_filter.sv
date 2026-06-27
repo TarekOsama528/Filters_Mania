ad5e5945fc112ecb0ca9b3adde20469502c46821
TarekOsama528 <67550435+TarekOsama528@users.noreply.github.com>
2026-06-27 05:54:31 +0300

    .

---
+++ LMS_Adaptive_Filter/LMS_adaptive_filter.sv
@@ -0,0 +1,7 @@
+module lms_adaptive_filter ();
+    parameter N = 10; // length of the filter
+    parameter M = 5 // number of taps in the filter
+    parameter DEL = 2; // delay between samples
+    
+
+endmodule
